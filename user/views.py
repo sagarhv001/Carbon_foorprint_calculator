bdf89f1da1e4be7e0c9d9418b6924eda969c024a
Sowmya K S <119122284+Sowmya-K-S@users.noreply.github.com>
2024-01-20 16:11:22 +0530

    fourth push
    discover page done
    calculator rendering

--- user/views.py
+++ user/views.py
@@ -1,14 +1,75 @@
 from django.shortcuts import render,redirect,HttpResponse
 
+
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
-def login(request):
-    return render(request,'login.html')
-
 def register(request):
-    return render(request, 'register.html')
+    global c_otp
+    # when we click on register option
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    
+    # when we click on register button
+    else:
+       
+        # checking whether the entered email is already used for registration
+        try:
+            # error occurs when there is no email match
+            # then control goes to except block
+
+            Patient.objects.get(email = request.POST['email'])
+            return render(request,'register.html',{'msg':"Email already registered, try using other Email"})
+        
+        except:
+            # validating password and confirm password
+            if request.POST['password'] == request.POST['cpassword']:
+                #  generating otp
+                global c_otp
+                c_otp = randint(100_000,999_999)
+
+                #extracting data from registration form
+                global reg_form_data 
+
+                reg_form_data = {
+                    "full_name" : request.POST['full_name'],
+                    "gender" : request.POST['gender'],
+                    "age" : request.POST['age'],
+                    "blood_group" : request.POST['blood_group'],
+                    "email" : request.POST['email'],
+                    "phoneno" : request.POST['phoneno'],
+                    "address" : request.POST['address'],
+                    "password" : request.POST['password'],
+                }
+
+                # sending the generated OTP via mail
+                subject = "Medick Registration"
+                message = f'Hello{reg_form_data["full_name"]}, Welcome to Medick. Your OTP is {c_otp}'
+                sender = settings.EMAIL_HOST_USER
+                receiver = [reg_form_data['email']]
+                send_mail(subject, message, sender, receiver)
+
+                #after sending OTP render the page to enter OTP
+                return render(request,'otp.html')
+
+            else:
+                return render(request, 'register.html',{'msg':"Both Passwords didn't match"})
+            
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        try:
+            # finding the record of person trying to login in database
+            session_user = User.objects.get(email = request.POST['email'])
+            if request.POST['password'] == session_user.password:
+                request.session['email'] = session_user.email
+                return redirect('home')
+            else:
+                return render(request, 'login.html', {'msg':'invalid password'})
+        except:
+            return render(request, 'login.html',{'msg': 'This email is not Registered !!'})
 
 def about(request):
     return render(request,'about.html')
@@ -21,3 +82,6 @@ def calculator(request):
 
 def otp(request):
     return render(request,'otp.html')
+
+def discover(request):
+    return render(request,'discover.html')
